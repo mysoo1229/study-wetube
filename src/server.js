c331adf37b2ec03896753a3ee7fa7473cbe9f82d
Crystal <crystalyoo29@interpark.com>
2025-06-22 15:34:23 +0900

    #3.3 Responses (11:23)

--- src/server.js
+++ src/server.js
@@ -4,9 +4,16 @@ const PORT = 4000;
 
 const app = express();
 
-const handleHome = () => console.log('I want to go home')
+const handleHome = (req, res) => {
+  return res.end();
+}
+
+const handleLogin = (req, res) => {
+  return res.send('Login here.');
+}
 
 app.get("/", handleHome);
+app.get("/login", handleLogin);
 
 const handleListening = () => console.log(`Server listening on port http://localhost:${PORT}`);
 
